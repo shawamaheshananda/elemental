8d7724c092bbd4488e50abb09dd1c01c2239bec4
Shawã Maheshananda <shawa.maheshananda@ibiete.org>
2026-04-29 20:56:21 -0300

    estrutura inicial arte-na-cura

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-echo "===> Atualizando código"
+
+cd /apps/arte-na-cura
+
+echo "===> Atualizando projeto arte-na-cura"
 git fetch origin
 git reset --hard origin/main
-echo "===> Deploy finalizado"
+
+echo "===> Subindo containers"
+docker compose up -d --build
+
+echo "===> Deploy concluído"
